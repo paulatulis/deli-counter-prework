014bbef49ca1137a29c914b409d51ad487045b45
Paula Tulis <tulis.paula@gmail.com>
2019-03-25 22:10:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ puts "Welcome, #{new_person}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
-  if katz_deli.empty? puts "The line is currently empty."
+  if katz_deli.empty? puts "There is nobody waiting to be served!"
   else 
     katz_deli.each |customer| do 
       puts "Currently serving #{customer}."
