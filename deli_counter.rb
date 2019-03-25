8ef3e381f8f6349d2f28d109ccada29c66d40a08
Paula Tulis <tulis.paula@gmail.com>
2019-03-25 22:09:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,3 +18,10 @@ def take_a_number(katz_deli, new_person)
 katz_deli.push(new_person)
 puts "Welcome, #{new_person}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.empty? puts "The line is currently empty."
+  else 
+    katz_deli.each |customer| customer.pop
+  end 
+end 
