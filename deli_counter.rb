e138a0b5d2fdb3879242899e803f5108794a7cef
Paula Tulis <tulis.paula@gmail.com>
2019-03-25 21:55:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,13 @@
 
-katz_deli=[]
+katz_deli = ["Logan", "Avi", "Spencer"]
 
 def line(katz_deli) 
-if katz_deli.empty? puts "The line is currently empty."
+if katz_deli.empty? 
+  puts "The line is currently empty."
 else 
+  current_line = "The line is currently:"
   katz_deli.each_with_index do 
     |names, position| 
-    current_line = "The line is currently:"
     current_line += " #{position.to_i+1}. #{names}" 
 end 
 puts "#{current_line}"
