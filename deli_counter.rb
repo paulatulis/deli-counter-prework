a07c2391d72b553b25ea546050a52fa0aaa51102
Paula Tulis <tulis.paula@gmail.com>
2019-03-25 21:36:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,11 @@
 
 def line(katz_deli)
 if katz_deli.empty? puts "The line is currently empty."
-  
-  katz_deli.each_with_index {|names| 
+else 
+  katz_deli.each_with_index do 
+    |names, position| 
+    position_plus_one = position + 1 
+    puts "The line is currently: #{position}. #{names} #{position}. #{names} #{position}. #{names}"
+end 
 end 
 end 
