7f0cd8c7995f6799bf8c78c5f05488382da31e06
Paula Tulis <tulis.paula@gmail.com>
2019-03-25 22:06:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,5 +16,5 @@ end
 
 def take_a_number(katz_deli, new_person)
 katz_deli.push(new_person)
-puts "Welcome, #{new_person}. You are number #{."
+puts "Welcome, #{new_person}. You are number #{katz_deli.length} in line."
 end
